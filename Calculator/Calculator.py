e64d4c457a7c292ed8640ff4debcd60ef432fa24
StanleyTarsiSun <51964620+StanleyTarsiSun@users.noreply.github.com>
2020-07-01 16:51:16 -0400

    Task： Added Example files

---
+++ Calculator/Calculator.py
@@ -0,0 +1,17 @@
+from Calculator.Subtraction import subtraction
+from Calculator.Addition import addition
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
