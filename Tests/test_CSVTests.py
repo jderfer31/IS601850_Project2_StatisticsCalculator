fb6ac349b9014c2f5d2a43924580e72ca8e81648
Cs586 <66033141+Cs586@users.noreply.github.com>
2020-07-03 14:42:54 -0400

    Fix: Updated coding for subtraction.csv

--- Tests/test_CSVTests.py
+++ Tests/test_CSVTests.py
@@ -1,20 +1,26 @@
 import unittest
-from CsvReader.CsvReader import CsvReader, ClassFactory
+from CsvReader import CsvReader
 from pprint import pprint
+from pathlib import Path
+
+
+def ClassFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
+
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.csv_reader = CsvReader('Tests/Data/employee_birthday.csv')
+        self.csv_reader = CsvReader('Tests/Data/subtraction.csv')
 
     def test_return_data_as_objects(self):
-        people = self.csv_reader.return_data_as_class('person')
-        self.assertIsInstance(people, list)
-        test_class = ClassFactory('person', self.csv_reader.data[0])
+        data = self.csv_reader.return_data_as_objects('value1')
+        self.assertIsInstance(data, list)
+        test_data = ClassFactory('value1', self.csv_reader.data[0])
 
-        for person in people:
-            self.assertEqual(person.__name__, test_class.__name__)
-            pprint(vars(people))
+        for value1 in data:
+            self.assertEqual(value1.__name__, test_data.__name__)
+
 
 if __name__ == '__main__':
     unittest.main()
