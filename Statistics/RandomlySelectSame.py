0a284fdfbe9823fbeb481806076386b90b6be018
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-07-05 23:49:10 -0400

    Task: Fixed random.select with seed

--- Statistics/RandomlySelectSame.py
+++ Statistics/RandomlySelectSame.py
@@ -3,10 +3,10 @@ import random
 
 nlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-random.seed(2)
-number_list = random.sample(nlist, 4)
+random.seed(5)
+number_list = random.sample(nlist, 1)
 print("number list", number_list)
 
-random.seed(2)
-number_list = random.sample(nlist, 4)
+random.seed(5)
+number_list = random.sample(nlist, 1)
 print("number list", number_list)
