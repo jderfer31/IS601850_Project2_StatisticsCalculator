89f604413180992c56dc6d36e4c5afa027e346c9
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-07-06 21:41:11 -0400

    Task: Added correction to ItemsWoutSeed.py

--- Statistics/ItemsWoutSeed.py
+++ Statistics/ItemsWoutSeed.py
@@ -1,12 +1,8 @@
 import random
 
+def items_without_seed(num):
+    nlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    number_list = random.sample(nlist, num)
+    return("number list", number_list)
 
-nlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-number_list = random.sample(nlist, 4)
-print("number list", number_list)
-
-
-number_list = random.sample(nlist, 4)
-print("number list", number_list)
+print(items_without_seed(4))
