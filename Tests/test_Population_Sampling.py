2961d34030432aa36edeb5f40738d3c5e0ce6f3a
StanleyTarsiSun <51964620+StanleyTarsiSun@users.noreply.github.com>
2020-07-08 17:56:31 -0400

    Fix: updated coding for unit test of Confidence_interval

--- Tests/test_Population_Sampling.py
+++ Tests/test_Population_Sampling.py
@@ -5,23 +5,21 @@ from Statistics.NListWithSeed import generator_int_and_float
 import math  # import Calculator
 import statistics  # import Statistics funcions
 import random
-from Statistics.Confidence_interval import confidence_interval_bottom_and_top
+from Statistics.Confidence_interval import confidence_interval_bottom
 from Statistics.Confidence_interval import result_confidence_interval_bottom_and_top
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         random.seed(5)
-        self.testData = random.randint(0, 10, 20)
-        self.statistics = Statistics()
-        self.confidence_interval_bottom_and_top = confidence_interval_bottom_and_top(80, 10, 30)
+        self.confidence_interval_bottom_and_top = confidence_interval_bottom(80, 10, 30)
         self.result = result_confidence_interval_bottom_and_top(80, 10, 30)
-
-    def test_something(self):
-        self.assertEqual(True, False)
+        pass
 
     def test_confidence_interval_bottom_and_top(self):
-        self.assertEqual(self.confidence_interval_bottom_and_top,self.result )
+        self.assertEqual(self.confidence_interval_bottom_and_top, self.result)
+
+
 
 
 if __name__ == '__main__':
