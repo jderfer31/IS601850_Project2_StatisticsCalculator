0f7bec934e7c82c8247ffbce6a4a03f12d7265a5
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-07-08 23:37:11 -0400

    Task: Added random to Statistics.py

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -5,6 +5,15 @@ from Statistics.Mode import mode
 from Statistics.Variance import variance
 from Statistics.Standard_Deviation import stddev
 from Statistics.Zscore import zscore
+from Statistics.RandomNumberWithSeed import random_integer
+from Statistics.ItemsWoutSeed import items_without_seed
+from Statistics.ItemsWithSeed import items_with_seed
+from Statistics.NListWithSeed import generator_int_and_float
+from Statistics.RandomItem import random_item
+from Statistics.RandomNumberWoutSeed import random_integer
+from Statistics.RandomlySelectSame import randomly_same
+
+
 
 class Statistics(Calculator):
 
@@ -28,5 +37,28 @@ class Statistics(Calculator):
         self.result = zscore(data, x)
         return self.result
 
+    def random_integer(self, data, x):
+        self.result = random_integer(data, x)
+        return self.result
+
+    def items_without_seed(self, data, x):
+        self.result = items_without_seed(data, x)
+        return self.result
+
+    def items_with_seed(self, data, x):
+        self.result = items_with_seed(data, x)
+        return self.result
+
+    def generator_int_and_float(self, data, x):
+        self.result = generator_int_and_float(data, x)
+        return self.result
+
+    def random_item(self, data, x):
+        self.result = random_item(data, x)
+        return self.result
+
+    def randomly_same(self, data, x):
+        self.result = randomly_same(data, x)
+        return self.result
 
 
