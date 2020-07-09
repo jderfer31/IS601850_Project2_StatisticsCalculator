25fe88fc83b1bc3bba1736b9c99641fb3737428e
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-07-08 20:57:29 -0400

    Task: Added random unittest to test_statistics.py

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 from numpy.random import seed
 from numpy.random import randint
 from Statistics.Statistics import Statistics
@@ -17,6 +18,30 @@ class MyTestCase(unittest.TestCase):
     def test_mean_calculator(self):
         mean = self.statistics.mean(self.testData)
         self.assertEqual(mean, 4.25)
+
+    def test_items_with_seed(self):
+        random.seed(5)
+        number_list = random.sample(nlist, num)
+        num = random.randint(0, 10)
+        self.assertEqual(items_with_seed(), num)
+
+    def test_items_Wout_seed(self):
+        number_list = random.sample(nlist, num)
+        num = random.randint(0, 10)
+        self.assertEqual(items_Wout_seed(), num)
+
+    def test_randomly_same(self):
+        random.seed(5)
+        number_list = random.sample(nlist, num)
+        num = random.randint(0, 10)
+        self.assertEqual(randomly_same(1), num)
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
