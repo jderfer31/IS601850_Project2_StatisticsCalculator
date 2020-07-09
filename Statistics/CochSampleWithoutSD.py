cb29dcef3bb47cffbd64013a7ba75b6160281228
StanleyTarsiSun <51964620+StanleyTarsiSun@users.noreply.github.com>
2020-07-08 20:38:24 -0400

    Feature: Created coding of CochSampleWithoutSD.py

--- Statistics/CochSampleWithoutSD.py
+++ Statistics/CochSampleWithoutSD.py
@@ -1,8 +1,48 @@
-import math # import Calculator
-import statistics # import Statistics funcions
+from Calculator.Subtraction import subtraction
+from Calculator.Division import division
+from Calculator.Multipule import mul
+from Calculator.Square import sq
+from Statistics.Standard_Deviation import stddev
+from Statistics.NListWithSeed import generator_int_and_float
+from Statistics.Mean import mean
+import math  # import Calculator
+import statistics  # import Statistics funcions
+import math  # import Calculator
+import statistics  # import Statistics funcions
 
 
-def cochran(proportion, probability, precision):
+def cochranwithNOsd(proportion, probability, precision):
+    p = probability
+    p1 = float(proportion / 100)
+    q = float(1 - p1)
+    me = precision
+    e = division(2, me)
+    if p == 80:
+        z = 1.282
+        return round(mul(mul(q, p1), sq(division(e, z))), 5)
+    elif p == 85:
+        z = 1.440
+        return round(mul(mul(q, p1), sq(division(e, z))), 5)
+    elif p == 90:
+        z = 1.645
+        return round(mul(mul(q, p1), sq(division(e, z))), 5)
+    elif p == 95:
+        z = 1.960
+        return round(mul(mul(q, p1), sq(division(e, z))), 5)
+    elif p == 99:
+        z = 2.576
+        return round(mul(mul(q, p1), sq(division(e, z))), 5)
+    elif p == 99.5:
+        z = 2.807
+        return round(mul(mul(q, p1), sq(division(e, z))), 5)
+    elif p == 99.9:
+        z = 3.291
+        return round(mul(mul(q, p1), sq(division(e, z))), 5)
+    else:
+        print("please select one interval")
+
+
+def result_cochranwithNOsd(proportion, probability, precision):
     p = probability
     print(p)
     p1 = float(proportion / 100)
@@ -38,4 +78,4 @@ def cochran(proportion, probability, precision):
         print("please select one interval")
 
 
-print(cochran(41, 95, 0.06))
+print(cochranwithNOsd(41, 95, 0.06))
